82736d294a1687e7021b567d4d172f5a1db75a58
hra7s <216ec1464@gmail.com>
2024-07-22 18:20:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let headEl= React.createElement("div",{},React.createElement("div",{},React.createElement("div",{},React.createElement("h1",{},"welcome to React"))))
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(headEl)
+
+{/* <div>
+    <div>
+        <div>
+            <div>
+                <h1>Welcome to HTMl</h1>
+            </div>
+        </div>
+    </div>
+</div> */}
