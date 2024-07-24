079c1628aa6f77db1f00e4a34880486825c76e62
hra7s <216ec1464@gmail.com>
2024-07-24 19:01:30 +0530

    vairables js

---
+++ JAVASCRIPT/BASICS/index.js
@@ -0,0 +1,46 @@
+
+
+// console.log("hello1")
+
+// console.log("hello2")
+
+//FUNCTION 
+
+//FUNCTION  DECLARATION 
+
+
+
+// function sum(a=0,b=1,c=3){
+
+//     console.log(a+b+c)
+
+// }
+
+// sum(10)
+// sum(4,5,6)  //function call or invoke 
+//4,5,6 --> arguments 
+//a,b,c --> parameters 
+
+
+//default parameters 
+
+
+// let name="Kohli"
+
+// function birthday(){
+//     console.log("cake party")
+//     console.log("dinner")
+//     console.log(name)
+// }
+
+// let home=birthday()
+
+
+
+function war(){
+
+    return "hello"
+
+}
+let c=war()
+console.log(c)
---
+++ JAVASCRIPT/VARIABLES/index.js
@@ -0,0 +1,64 @@
+
+
+// var a=10 
+
+//we have declare "a" variable and assign a value of 10 in to it 
+
+// console.log(a)
+
+// var a=1
+
+//we have redeclare "a" variable and assign a value of 1 in to it 
+// console.log(a)
+
+//var --> redeclaration is possible 
+
+// var j=12
+//we have declare "j" variable and assign a value of 12 in to it 
+
+// j=100
+
+//we have reassign a value of 100 in to j 
+
+// console.log(j)
+
+//reassignment is also possible with var 
+
+
+// var k;
+// //at the time of declaration value assignment is not mandatory 
+// k=19 
+// console.log(k)/
+
+
+
+//suppose if you are design a game 
+
+// var score=50  
+
+// if(score===50){
+//     console.log("Player 1 wins")
+// }
+
+
+// var score=200
+
+//let and const 
+
+//redeclaration is not possible but reassigment is possible 
+// let q;
+// q=200
+// console.log(q)
+
+//const 
+
+// const a=100
+// const a=200 
+// console.log(a)
+//both redeclaration and reassignment is not possible 
+
+const a=100
+
+
+// a=200 
+console.log(a)
