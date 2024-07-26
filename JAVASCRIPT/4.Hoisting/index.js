aa797e85bdb8177b5231bf58a1364b678f212cd6
hra7s <216ec1464@gmail.com>
2024-07-26 13:39:46 +0530

    closures and lexical

--- JAVASCRIPT/4.Hoisting/index.js
+++ JAVASCRIPT/4.Hoisting/index.js
@@ -1,5 +1,4 @@
 
-
 //not defined vs undefined
 // var t;
 // console.log(t) //undefined
@@ -8,21 +7,21 @@
 
 //GEC 
 
-var f=100
+// var f=100
 
-function sum(){
-    var g=200
-    function war(){
-        var e=300
-        function qer(){
+// function sum(){
+//     var g=200
+//     function war(){
+//         var e=300
+//         function qer(){
           
-            console.log(f+g+e)
-        }
-        qer()
-    }
-    war()
-}
-sum() 
+//             console.log(f+g+e)
+//         }
+//         qer()
+//     }
+//     war()
+// }
+// sum() 
 
 //here sum() is code execution 
 
@@ -33,3 +32,51 @@ sum()
 // console.log(d)
 
 // var d=9
+
+// console.log(d)
+// let d=10
+
+
+//Hoisting --> Variables declarations with var and function declarations  are moved to the top of the scope before code execution 
+
+// sum()
+
+
+// function sum(){
+
+//     console.log("hello")
+
+// }
+
+
+
+
+//function expressions and arrow functions --> hoisting not possible 
+// play()
+
+// let play=function(){
+
+//     console.log("hello")
+// }
+
+
+//arrow function 
+
+// let war=()=>{
+//     return "hello"
+// }
+// console.log(war())
+
+// let war=()=> "hello"
+// console.log(war())
+
+// let war=(a,b)=> a+b
+// console.log(war(4,5))
+
+
+// let war=a=> a
+// console.log(war(4))
+
+
+// let war=()=> "a"
+// console.log(war())
---
+++ JAVASCRIPT/5.Lexical&Closures/index.js
@@ -0,0 +1,34 @@
+
+
+
+//lexical scope --> inner function surrounding scope 
+// function sum(){
+
+//     let d=100 
+//     function war(){
+//         let a=100
+//         console.log(a+d)  
+//     }
+//     war()
+
+// }
+
+// sum()
+
+//lexical scope 
+
+
+//Closures 
+
+
+//the function which bind together to its lexical scope forms a closure 
+// function sum(){
+// const r=17 
+// return function war(){
+//     let a=100
+//     console.log(a+r)
+// }
+// }
+
+// const w=sum()
+// w()
