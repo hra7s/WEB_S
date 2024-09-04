4285fb0d0ce41073bf77a807e176a1de2b98dfd3
hra7s <216ec1464@gmail.com>
2024-09-04 11:52:23 +0530

    http methods

---
+++ JAVASCRIPT/27.HTTP_Methods/index.js
@@ -0,0 +1,46 @@
+//what is url  -->? 
+
+
+//url 
+
+//http methods 
+
+//HTTP REQUEST (CLIENT)
+//HTTP RESPONSE (SERVER)
+
+
+//HTTP REQUEST --> 
+
+//1. startline    -->1.1  url 1.2 http methods 1.3 http version 
+//2. headers (additional information)
+//3. Body (main data)
+//1.2.1   CRUD (CREATE READ UPDATE DELETE)
+// CREATE (POST)  READ (GET) UPDATE (PUT) DELETE (DELETE)
+
+
+let data={
+    // name:"React",
+    email:"react123@gmail.com",
+    // status:"active",
+    // gender:"female"
+}
+let url="https://gorest.co.in/public/v2/users/7382969"
+let options={
+    method:"DELETE",
+    headers:{
+        "Content-Type" : "application/Json",
+        Accept:"application/Json",
+        Authorization: "Bearer 20955982edea6a3113dcaba5796abdd08fb6b995a0c0b66b445532389edfbf93"
+    },
+    body: JSON.stringify(data)
+}
+
+
+fetch(url,options)
+.then((res)=>{return res.json()})
+.then((data)=> {console.log(data)})
+.catch((e)=> console.log(e))
+
+//promise chaining 
+
+//https://gorest.co.in/ 
